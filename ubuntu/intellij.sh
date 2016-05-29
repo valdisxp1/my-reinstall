70cf70d4574215fb3f41288c529bb42b945c2967
Valdis <valdisxp1@gmail.com>
2016-05-29 18:21:37 +0300

    intellij .. almost

---
+++ ubuntu/intellij.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# TODO get latest direct link from this page https://www.jetbrains.com/idea/download/download-thanks.html?platform=linux&code=IIC
+wget https://download.jetbrains.com/idea/ideaIC-2016.1.2b.tar.gz &&
+tar -zxvf ideaIC-2016.*.tar.gz &&
+rm ideaIC-2016.*.tar.gz
+# TODO create the desktop item
+
+# @test
