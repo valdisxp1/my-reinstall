b5d48004fcefe057f2719ba8a5fce184786236d3
Valdis <valdisxp1@gmail.com>
2016-05-29 18:32:27 +0300

    JProfiler 9.1.1

---
+++ ubuntu/jprofiler.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# From https://www.ej-technologies.com/download/jprofiler/files
+wget http://download-keycdn.ej-technologies.com/jprofiler/jprofiler_linux_9_1_1.sh &&
+sudo sh jprofiler_linux_9_1_1.sh
+
+# @test
