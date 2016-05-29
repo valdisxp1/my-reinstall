8c4e0eac3ba50a94b2567ba82d16f711ea57d73b
Valdis <valdisxp1@gmail.com>
2016-05-29 17:40:46 +0300

    install oracle jdk 8

---
+++ ubuntu/oracle_java.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections &&
+sudo add-apt-repository -y ppa:webupd8team/java 
+sudo apt-get install -y oracle-java8-installer
+
+# @test
+java --version
+javac --version
