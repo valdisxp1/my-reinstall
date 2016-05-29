4cf7558dde8c5155f659d37b0a182aae26eb9426
Valdis <valdisxp1@gmail.com>
2016-05-29 17:25:45 +0300

    what key I set the passprase for

--- ubuntu/git.sh
+++ ubuntu/git.sh
@@ -4,7 +4,9 @@ git config --global user.name "Valdis" &&
 git config --global user.email valdisxp1@gmail.com &&
 git config --global push.default simple &&
 mkdir -p ~/.ssh/ &&
+echo "Github key" &&
 ssh-keygen -b 2048 -f ~/.ssh/github &&
+echo "Bitbucket key" &&
 ssh-keygen -b 2048 -f ~/.ssh/bitbucket
 # @test
 git --version
