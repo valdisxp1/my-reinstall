189ed900adbcc959c354afa255f5b17b854cec13
Valdis <valdisxp1@gmail.com>
2016-05-29 17:55:20 +0300

    source

--- ubuntu/oracle_java.sh
+++ ubuntu/oracle_java.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# From https://github.com/dockerfile/java/blob/master/oracle-java8/Dockerfile
+# From http://www.webupd8.org/2012/09/install-oracle-java-8-in-ubuntu-via-ppa.html
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections &&
 sudo add-apt-repository -y ppa:webupd8team/java &&
 sudo apt-get install -y oracle-java8-installer
