7294c3bd1dde2b42b9da2dbafccba8ef15f6dc61
Valdis <valdisxp1@gmail.com>
2016-06-04 15:35:16 +0300

    removed wrong source

--- ubuntu/vagrant.sh
+++ ubuntu/vagrant.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# From https://www.ej-technologies.com/download/jprofiler/files
 wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb -O /tmp/vagrant_1.8.1.deb &&
 sudo dpkg -i /tmp/vagrant_1.8.1.deb
 rm /tmp/vagrant_1.8.1.deb
