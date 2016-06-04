5e1810168e4b834003cb4dd95e2ae24164fd2920
Valdis <valdisxp1@gmail.com>
2016-06-04 15:52:22 +0300

    fixed node install

--- ubuntu/node.sh
+++ ubuntu/node.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # From https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
 wget -qO- https://deb.nodesource.com/setup | sudo bash - &&
-sudo apt-get install -y nodejs
+sudo apt-get install -y nodejs npm
 
 # @test
-node --version
+nodejs --version
 npm --version
