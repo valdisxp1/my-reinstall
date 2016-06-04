cb62c91c0e3718a3492ae9ce547c2441c50b03a9
Valdis <valdisxp1@gmail.com>
2016-06-04 10:36:51 +0300

    pip is wierd, complains whether sudo or not

--- ubuntu/python.sh
+++ ubuntu/python.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get install -y python python-apt &&
-wget -qO- https://bootstrap.pypa.io/get-pip.py | python &&
-pip install docker-py
+wget -qO- https://bootstrap.pypa.io/get-pip.py | sudo python &&
+sudo pip install docker-py
 # @test
 python --version
 pip --version
