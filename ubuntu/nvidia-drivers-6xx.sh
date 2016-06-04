ff182f4f2200fb759096ab73f98394c57c962d71
Valdis <valdisxp1@gmail.com>
2016-06-04 15:28:23 +0300

    automate

--- ubuntu/nvidia-drivers-6xx.sh
+++ ubuntu/nvidia-drivers-6xx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # https://launchpad.net/~graphics-drivers/+archive/ubuntu/ppa
-sudo add-apt-repository ppa:graphics-drivers/ppa &&
+sudo add-apt-repository -y ppa:graphics-drivers/ppa &&
 sudo apt-get update &&
-sudo apt-get install nvidia-304 # 660 ti and 630M
+sudo apt-get install -y nvidia-304 # 660 ti and 630M
 # @test
