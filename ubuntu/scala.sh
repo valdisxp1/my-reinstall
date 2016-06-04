e8ced6320d34084105ae3da66dd5bbebcda133d5
Valdis <valdisxp1@gmail.com>
2016-06-04 11:13:03 +0300

    scala sbt and console project

---
+++ ubuntu/scala.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# sbt from http://www.scala-sbt.org/download.html, modified
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt.list &&
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823 &&
+sudo apt-get update &&
+sudo apt-get install -y sbt &&
+wget http://downloads.lightbend.com/scala/2.11.8/scala-2.11.8.deb -O /tmp/scala-2.11.8.deb &&
+sudo dpkg -i /tmp/scala-2.11.8.deb &&
+git clone https://github.com/valdisxp1/sbt-scala.git ~/sbt-scala
+rm /tmp/scala-2.11.8.deb
+# @preload
+# downloads sbt itself
+sbt sbtVersion
+# @test
+sbt sbtVersion
+scala -version
