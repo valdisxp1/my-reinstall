2979c6bb2eef4efbf0fb75af4b90ae45d39c8203
Valdis Adamsons <valdisxp1@gmail.com>
2016-06-05 17:39:45 +0300

    notepad install exe to use temp files

--- windows10/notepad++.ps1
+++ windows10/notepad++.ps1
@@ -1,2 +1,3 @@
-wget https://notepad-plus-plus.org/repository/6.x/6.9.2/npp.6.9.2.Installer.exe -OutFile npp.6.9.2.Installer.exe
-.\npp.6.9.2.Installer.exe /S
+wget https://notepad-plus-plus.org/repository/6.x/6.9.2/npp.6.9.2.Installer.exe -O $env:temp\npp.6.9.2.Installer.exe
+& $env:temp\npp.6.9.2.Installer.exe /S
+rm $env:temp\npp.6.9.2.Installer.exe
