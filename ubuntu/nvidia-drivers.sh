d3cbcd1a6f13ec3faf91ad3f4281ca5d7969768f
Valdis <valdisxp1@gmail.com>
2016-06-11 10:40:44 +0300

    Older driver versions don't work well with gnome.\n Using the lastest recomended stable version (364)

--- ubuntu/nvidia-drivers.sh
+++ ubuntu/nvidia-drivers.sh
@@ -2,5 +2,5 @@
 # https://launchpad.net/~graphics-drivers/+archive/ubuntu/ppa
 sudo add-apt-repository -y ppa:graphics-drivers/ppa &&
 sudo apt-get update &&
-sudo apt-get install -y nvidia-304 # 660 ti and 630M
+sudo apt-get install -y nvidia-364
 # @test
