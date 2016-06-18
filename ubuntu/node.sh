69a64a7f6755d5382f63f4d9789c7a4606e74fcf
Valdis <valdisxp1@gmail.com>
2016-06-18 21:56:20 +0200

    grunt install

--- ubuntu/node.sh
+++ ubuntu/node.sh
@@ -2,7 +2,12 @@
 # From https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
 wget -qO- https://deb.nodesource.com/setup | sudo bash - &&
 sudo apt-get install -y nodejs npm
+# From https://github.com/nodejs/node-v0.x-archive/issues/3911
+# From http://gruntjs.com/getting-started
+sudo npm install -g grunt-cli &&
+sudo ln -s /usr/bin/nodejs /usr/bin/node
 
 # @test
 nodejs --version
 npm --version
+grunt --version
