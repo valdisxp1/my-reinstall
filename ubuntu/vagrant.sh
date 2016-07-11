82e76ae35d8b3b9b3bb062d15e2b521b84c7c416
Valdis <valdisxp1@gmail.com>
2016-07-11 17:17:10 +0300

    kvm and vultr providers

--- ubuntu/vagrant.sh
+++ ubuntu/vagrant.sh
@@ -2,7 +2,9 @@
 wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb -O /tmp/vagrant_1.8.1.deb &&
 sudo dpkg -i /tmp/vagrant_1.8.1.deb &&
 vagrant plugin install vagrant-scp &&
-vagrant plugin install vagrant-aws
+vagrant plugin install vagrant-aws &&
+vagrant plugin install vagrant-libvirt &&
+vagrant plugin install vagrant-vultr
 rm /tmp/vagrant_1.8.1.deb
 
 # @test
