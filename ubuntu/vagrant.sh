09973c4deb8578de61aca8ebf1a99c7284a7f66b
Valdis <valdisxp1@gmail.com>
2016-07-12 19:41:05 +0300

    kvm for vagrant

--- ubuntu/vagrant.sh
+++ ubuntu/vagrant.sh
@@ -3,9 +3,15 @@ wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb -O /t
 sudo dpkg -i /tmp/vagrant_1.8.1.deb &&
 vagrant plugin install vagrant-scp &&
 vagrant plugin install vagrant-aws &&
-vagrant plugin install vagrant-libvirt &&
 vagrant plugin install vagrant-vultr
+
+#KVM / libvirt
+sudo apt-get install -y libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev &&
+sudo gem install ruby-libvirt -v '0.6.0' &&
+vagrant plugin install vagrant-libvirt 
+
 rm /tmp/vagrant_1.8.1.deb
 
 # @test
 vagrant --version
+vagrant plugin list
