94da8fad7c7f0ebd90004da7c2d4f55a930be521
Valdis <valdisxp1@gmail.com>
2017-08-02 20:09:00 +0300

    comment

--- ubuntu/docker.sh
+++ ubuntu/docker.sh
@@ -3,7 +3,7 @@
 wget -qO- https://get.docker.com/ | sh &&
 sudo usermod -aG docker valdis &&
 sudo service docker start
-https://docs.docker.com/compose/install/
+# From https://docs.docker.com/compose/install/
 sudo "curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
 # @test
 docker --version
