96e6aee410a3bdbddd914419fa48bcbea9e833e5
Valdis <valdisxp1@gmail.com>
2017-08-02 20:12:43 +0300

    just use apt

--- ubuntu/docker.sh
+++ ubuntu/docker.sh
@@ -4,7 +4,7 @@ wget -qO- https://get.docker.com/ | sh &&
 sudo usermod -aG docker valdis &&
 sudo service docker start
 # From https://docs.docker.com/compose/install/
-sudo "curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo apt install -y docker-compose
 # @test
 docker --version
 docker-compose --version
