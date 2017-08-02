1eba2cf0dcd4bc81acc3b954206855bd42f8df93
Valdis <valdisxp1@gmail.com>
2017-08-02 18:31:07 +0300

    docker and compose

--- ubuntu/docker.sh
+++ ubuntu/docker.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-# From https://docs.docker.com/engine/installation/linux/ubuntulinux/
-sudo apt-get update &&
-sudo apt-get install -y apt-transport-https ca-certificates linux-image-extra-$(uname -r) &&
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D &&
-sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-$(lsb_release -cs) main" > /etc/apt/sources.list.d/docker.list' &&
-sudo apt-get update &&
-sudo apt-get install -y docker-engine &&
+# From https://get.docker.com/
+wget -qO- https://get.docker.com/ | sh &&
 sudo usermod -aG docker valdis &&
 sudo service docker start
+https://docs.docker.com/compose/install/
+sudo "curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
 # @test
 docker --version
-
+docker-compose --version
