7e74f9a7b3ed018bf42ca0812e89d082bb9f30ed
Valdis <valdisxp1@gmail.com>
2017-08-02 18:30:53 +0300

    newer virtualbox

--- ubuntu/virtualbox.sh
+++ ubuntu/virtualbox.sh
@@ -4,6 +4,6 @@ echo "deb http://download.virtualbox.org/virtualbox/debian `lsb_release -cs` con
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add - &&
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add - &&
 sudo apt-get update &&
-sudo apt-get install -y virtualbox-5.0
+sudo apt-get install -y virtualbox-5.1
 # @test
 vboxmanage --version
