6b6c8d3174b586d5439cd0c2f18347f6fcf8a3d7
Valdis <valdisxp1@gmail.com>
2017-08-02 19:31:22 +0300

    vargrant new version

--- ubuntu/vagrant.sh
+++ ubuntu/vagrant.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb -O /tmp/vagrant_1.8.1.deb &&
-sudo apt-get install -y /tmp/vagrant_1.8.1.deb &&
+VAGRANT_VERSION=1.9.7
+wget https://releases.hashicorp.com/vagrant/$VAGRANT_VERSION/vagrant_${VAGRANT_VERSION}_x86_64.deb -O /tmp/vagrant_$VAGRANT_VERSION.deb &&
+sudo apt-get install -y /tmp/vagrant_$VAGRANT_VERSION.deb &&
 vagrant plugin install vagrant-scp &&
 vagrant plugin install vagrant-aws &&
 vagrant plugin install vagrant-vultr
@@ -10,7 +11,7 @@ sudo apt-get install -y libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev 
 sudo gem install ruby-libvirt -v '0.6.0' &&
 vagrant plugin install vagrant-libvirt 
 
-rm /tmp/vagrant_1.8.1.deb
+rm /tmp/vagrant_$VAGRANT_VERSION.deb
 
 # @test
 vagrant --version
