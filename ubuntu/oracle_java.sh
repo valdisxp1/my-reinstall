08edf9a43579c7c694dee97f8425878609c40678
Valdis Adamsons <valdisxp1@gmail.com>
2018-02-14 16:23:16 +0100

    Update oracle_java.sh
    Adding missing apt update
    using apt instead of apt-get

--- ubuntu/oracle_java.sh
+++ ubuntu/oracle_java.sh
@@ -3,7 +3,8 @@
 # From http://www.webupd8.org/2012/09/install-oracle-java-8-in-ubuntu-via-ppa.html
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections &&
 sudo add-apt-repository -y ppa:webupd8team/java &&
-sudo apt-get install -y oracle-java8-installer
+sudo apt update &&
+sudo apt install -y oracle-java8-installer
 
 # @test
 java -version
