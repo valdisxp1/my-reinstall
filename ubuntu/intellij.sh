329726ae5160cd32b310546bdf0bdf2367271c32
Valdis <valdisxp1@gmail.com>
2018-03-31 15:19:05 +0200

    intellij new version

--- ubuntu/intellij.sh
+++ ubuntu/intellij.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # TODO get latest direct link from this page https://www.jetbrains.com/idea/download/download-thanks.html?platform=linux&code=IIC
-wget https://download.jetbrains.com/idea/ideaIC-2017.3.3.tar.gz -O /tmp/ideaIC.tar.gz  &&
+wget https://download.jetbrains.com/idea/ideaIC-2018.1.tar.gz -O /tmp/ideaIC.tar.gz  &&
 tar -zxvf /tmp/ideaIC.tar.gz -C ~
 rm /tmp/ideaIC*.tar.gz
 # TODO create the desktop item
