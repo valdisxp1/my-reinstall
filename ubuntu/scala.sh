38762ff68c00e41cd06c89a5a56ae02831a4b04e
Valdis <valdisxp1@gmail.com>
2018-12-10 19:17:41 +0100

    update to 2.12.8

--- ubuntu/scala.sh
+++ ubuntu/scala.sh
@@ -4,10 +4,10 @@ echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee /etc/apt/sources.list.
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823 &&
 sudo apt-get update &&
 sudo apt-get install -y sbt &&
-wget http://downloads.lightbend.com/scala/2.11.8/scala-2.11.8.deb -O /tmp/scala-2.11.8.deb &&
-sudo apt-get install -y /tmp/scala-2.11.8.deb &&
+wget http://downloads.lightbend.com/scala/2.12.8/scala-2.12.8.deb -O /tmp/scala-2.12.8.deb &&
+sudo apt-get install -y /tmp/scala-2.12.8.deb &&
 git clone https://github.com/valdisxp1/sbt-scala.git ~/sbt-scala
-rm /tmp/scala-2.11.8.deb
+rm /tmp/scala-2.12.8.deb
 # @preload
 # downloads sbt itself
 sbt sbtVersion
