a7d47a7f835d9a1373d5d03e9fa331c70409014e
Valdis <valdisxp1@gmail.com>
2019-01-11 19:25:58 +0100

    enable automated git uploads fix

--- ubuntu/git.sh
+++ ubuntu/git.sh
@@ -3,6 +3,6 @@ sudo apt-get update && sudo apt-get install -y git rabbitvcs-nautilus rabbitvcs-
 git config --global user.name "Valdis" &&
 git config --global user.email valdisxp1@gmail.com &&
 git config --global push.default simple
-git config --global alias.up '!GIT_MERGE_AUTOEDIT=no; git add . && git commit -m automated && git pull && git push'
+git config --global alias.up '! git add . && git commit -m automated && !GIT_MERGE_AUTOEDIT=no git pull && git push'
 # @test
 git --version
